92588e70713805decdccb83f8632c63d132c8cbc
Usama Ansari <usmslm102@gmail.com>
2025-03-11 13:40:34 +0530

    Add PostHog integration for analytics tracking in main component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Resume from './Resume.tsx'
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: 'https://us.i.posthog.com',
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <PostHogProvider
+      apiKey={'phc_dITUDVdq00LQdmnKc6rS1Pr5uxDRWnVqcTThNOb7Pvl'}
+      options={options}>
     <Resume />
+    </PostHogProvider>
   </StrictMode>,
 )
